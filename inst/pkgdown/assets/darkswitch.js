091df46717c8901bce7c16ca12f6cf5e3f37b847
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-12-15 18:27:31 +0100

    added the script

---
+++ inst/pkgdown/assets/darkswitch.js
@@ -0,0 +1,47 @@
+// you can use app's unique identifier here
+const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
+
+const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+// you can change this url as needed
+const LIGHT_THEME_PATH = "https://bootswatch.com/3/faltly/bootstrap.css";
+const DARK_THEME_PATH = "https://bootswatch.com/3/darkly/bootstrap.css";
+
+const DARK_STYLE_LINK = document.getElementById("dark-theme-style");
+const THEME_TOGGLER = document.getElementById("theme-toggler");
+
+let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
+
+// check if user has already selected dark theme earlier
+if (isDark) {
+  enableDarkTheme();
+} else {
+  disableDarkTheme();
+}
+
+
+/**
+ * Apart from toggling themes, this will also store user's theme preference in local storage.
+ * So when user visits next time, we can load the same theme.
+ *
+ */
+function toggleTheme() {
+  isDark = !isDark;
+  if (isDark) {
+    enableDarkTheme();
+  } else {
+    disableDarkTheme();
+  }
+  const META = { isDark };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(META));
+}
+
+function enableDarkTheme() {
+  DARK_STYLE_LINK.setAttribute("href", DARK_THEME_PATH);
+  THEME_TOGGLER.innerHTML = "🌙 Dark";
+}
+
+function disableDarkTheme() {
+  DARK_STYLE_LINK.setAttribute("href", LIGHT_THEME_PATH);
+  THEME_TOGGLER.innerHTML = "🌞 Light";
+}
