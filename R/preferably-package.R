5ddc18c9afd2c6402f024a42384f94b8cd95b9c9
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-01-16 20:07:45 +0100

    added the package specification file

---
+++ R/preferably-package.R
@@ -0,0 +1,19 @@
+#' @title
+#' preferably
+#' 
+#' @description
+#' An accessible template for pkgdown websites with dynamic light/dark mode support
+#'
+#' @details
+#' This is an accessible template for 'pkgdown'. It
+#' uses two bootstrap themes, Flatly and Darkly and utilizes the
+#' 'prefers-color-scheme' CSS variable to automatically serve either of
+#' the two based on user’s operating system setting, or allowing them to
+#' manually toggle between them.
+#'
+#' @md
+#' @name preferably
+#' @docType package
+#' @author Amir Masoud Abdol (i@amirmasoudabdol.name)
+#' @keywords internal
+"_PACKAGE"
